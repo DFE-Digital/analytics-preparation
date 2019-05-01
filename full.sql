decef026a03fe76f7dd286ed5400a87cc90c1bfa
Peter Yates <peter.yates@graphia.co.uk>
2019-05-01 15:53:08 +0100

    Fix unnest, ensure empty arrays aren't discarded!

--- full.sql
+++ full.sql
@@ -1,25 +1,30 @@
-CREATE OR REPLACE view bookings_school_searches_cleansed (
+CREATE OR REPLACE VIEW bookings_school_searches_cleansed AS (
 	SELECT
 		bookings_school_searches.id,
 		bookings_school_searches.location,
 		bookings_school_searches.radius,
-		unnest(bookings_school_searches.subjects) AS subject_id,
-		unnest(bookings_school_searches.phases) AS phase_id,
 		bookings_school_searches.max_fee,
 		bookings_school_searches.page,
-		bookings_school_searches.number_of_results,
-		st_x(bookings_school_searches.coordinates::geometry) AS x,
+		bookings_school_searches.number_of_results,    st_x(bookings_school_searches.coordinates::geometry) AS x,
 		st_y(bookings_school_searches.coordinates::geometry) AS y,
-		bookings_school_searches.created_at
+		bookings_school_searches.created_at,
+		subject_id,
+		phase_id
 	FROM
 		bookings_school_searches
+	LEFT JOIN LATERAL
+		unnest(bookings_school_searches.subjects) subject_id
+		ON true
+	LEFT JOIN LATERAL
+		unnest(bookings_school_searches.phases) phase_id
+		ON true
 );
 CREATE OR REPLACE VIEW bookings_schools_cleansed AS (
 	SELECT
 		bookings_schools.id,
 		bookings_schools.name,
-		st_x(bookings_schools.coordinates::geometry) AS st_x,
-		st_y(bookings_schools.coordinates::geometry) AS st_y,
+		st_x(bookings_schools.coordinates::geometry) AS x,
+		st_y(bookings_schools.coordinates::geometry) AS y,
 		bookings_schools.created_at,
 		bookings_schools.updated_at,
 		bookings_schools.urn,
--- scripts/create_or_replace_bookings_school_searches_cleansed.sql
+++ scripts/create_or_replace_bookings_school_searches_cleansed.sql
@@ -1,16 +1,21 @@
-CREATE OR REPLACE view bookings_school_searches_cleansed (
+CREATE OR REPLACE VIEW bookings_school_searches_cleansed AS (
 	SELECT
 		bookings_school_searches.id,
 		bookings_school_searches.location,
 		bookings_school_searches.radius,
-		unnest(bookings_school_searches.subjects) AS subject_id,
-		unnest(bookings_school_searches.phases) AS phase_id,
 		bookings_school_searches.max_fee,
 		bookings_school_searches.page,
-		bookings_school_searches.number_of_results,
-		st_x(bookings_school_searches.coordinates::geometry) AS x,
+		bookings_school_searches.number_of_results,    st_x(bookings_school_searches.coordinates::geometry) AS x,
 		st_y(bookings_school_searches.coordinates::geometry) AS y,
-		bookings_school_searches.created_at
+		bookings_school_searches.created_at,
+		subject_id,
+		phase_id
 	FROM
 		bookings_school_searches
+	LEFT JOIN LATERAL
+		unnest(bookings_school_searches.subjects) subject_id
+		ON true
+	LEFT JOIN LATERAL
+		unnest(bookings_school_searches.phases) phase_id
+		ON true
 );
